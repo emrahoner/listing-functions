aec5bbda8ab578621eabc271f8472e240d5397fc
Emrah Oner <emrahoner@gmail.com>
2020-10-15 21:46:09 +1100

    chore: project is initiated

---
+++ docker-run.sh
@@ -0,0 +1,8 @@
+echo "Building and running $1 on port $2"
+
+if [ "$(docker ps -aq -f name=$1)" ]; then
+    docker rm --force $1
+fi
+
+docker image build -f $3 -t $1:local .
+docker run -p $2:80 -detach --env-file .env --name $1 $1:local
